62c8be9c28dae7ad955d7e590f2e0f559a4a15c3
Kerwin <kerwin.parada@gmail.com>
2019-11-13 15:17:06 -0600

    Updated Jersey and Order associations.

--- app/models/jersey.rb
+++ app/models/jersey.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Jersey < ApplicationRecord
+  has_many :jersey_orders
+  has_many :orders, through: :jersey_orders
+
   belongs_to :team
 
   has_one_attached :image
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class Order < ApplicationRecord
+  has_many :jersey_orders
+  has_many :jerseys, through: :jersey_orders
 end
