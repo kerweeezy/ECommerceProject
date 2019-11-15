1c2869bbe982382e05085459822e6519a6177937
Kerwin <kerwin.parada@gmail.com>
2019-11-15 00:30:44 -0600

    Added some navigation

--- app/controllers/team_controller.rb
+++ app/controllers/team_controller.rb
@@ -1,5 +1,11 @@
+# frozen_string_literal: true
+
 class TeamController < ApplicationController
   def index
     @teams = Team.all
+  end
+
+  def show
+    @team = Team.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'team/index'
-  get 'jerseys/index'
-  get 'jerseys/show'
-
-  resources :jerseys
+  resources :team, only: %i[index show]
+  resources :jerseys, only: %i[index show]
 
   root 'team#index'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
