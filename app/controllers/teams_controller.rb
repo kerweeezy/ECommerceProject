1084c1d2dcf231516f61f0a7b8017f3cc905ee0a
Kerwin <kerwin.parada@gmail.com>
2019-11-28 03:16:18 -0600

    Styling changes

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -6,16 +6,16 @@ class TeamsController < ApplicationController
   def index
     @pagy, @teams = pagy(Team.all, items: 5)
 
-    add_breadcrumb ' home ', :root_path
-    add_breadcrumb ' teams ', :teams_path
+    add_breadcrumb 'home', :root_path
+    add_breadcrumb '// teams', :teams_path
   end
 
   def show
     @team = Team.find(params[:id])
 
-    add_breadcrumb ' home ', :root_path
-    add_breadcrumb ' teams ', :teams_path
-    add_breadcrumb @team.name, :team_path
+    add_breadcrumb 'home', :root_path
+    add_breadcrumb '// teams', :teams_path
+    add_breadcrumb ' // ' + @team.name, :team_path
   end
 
   def search_results
